80e186f37552f9dda6501e5c1ec1027c9f7b9ce0
TangoMango <jea@purdue.edu>
2014-04-07 19:22:11 -0400

    Added DAC test code.

--- ccs/Debug/main.pp
+++ ccs/Debug/main.pp
@@ -1,19 +1,31 @@
 # FIXED
 
 main.obj: ../main.c
+main.obj: C:/StellarisWare/inc/hw_ints.h
+main.obj: C:/StellarisWare/inc/hw_i2c.h
 main.obj: C:/StellarisWare/inc/hw_memmap.h
 main.obj: C:/StellarisWare/inc/hw_types.h
 main.obj: C:/StellarisWare/driverlib/sysctl.h
 main.obj: C:/StellarisWare/driverlib/gpio.h
 main.obj: C:/StellarisWare/driverlib/pin_map.h
 main.obj: C:/StellarisWare/driverlib/debug.h
+main.obj: C:/StellarisWare/driverlib/fpu.h
 main.obj: C:/StellarisWare/driverlib/flash.h
+main.obj: C:/StellarisWare/driverlib/interrupt.h
+main.obj: C:/StellarisWare/driverlib/rom.h
+main.obj: C:/StellarisWare/driverlib/i2c.h
 
 ../main.c: 
+C:/StellarisWare/inc/hw_ints.h: 
+C:/StellarisWare/inc/hw_i2c.h: 
 C:/StellarisWare/inc/hw_memmap.h: 
 C:/StellarisWare/inc/hw_types.h: 
 C:/StellarisWare/driverlib/sysctl.h: 
 C:/StellarisWare/driverlib/gpio.h: 
 C:/StellarisWare/driverlib/pin_map.h: 
 C:/StellarisWare/driverlib/debug.h: 
+C:/StellarisWare/driverlib/fpu.h: 
 C:/StellarisWare/driverlib/flash.h: 
+C:/StellarisWare/driverlib/interrupt.h: 
+C:/StellarisWare/driverlib/rom.h: 
+C:/StellarisWare/driverlib/i2c.h: 
--- ccs/main.c
+++ ccs/main.c
@@ -1,13 +1,26 @@
+#include "inc/hw_ints.h"
+#include "inc/hw_i2c.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/debug.h"
+#include "driverlib/fpu.h"
 #include "driverlib/flash.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/rom.h"
+#include "driverlib/i2c.h"
+
+// Slave Port
+#define GPIO_PA6_I2C1SCL		0x00001803
+#define GPIO_PA7_I2C1SDA		0x00001C03
+#define SLAVE_ADDRESS 			0x4C // 10011 A0 0
 
 void testHeartBeat(void);
 void testFlash(void);
+void testDAC(void);
+void send_data(int);
 
 int main(void)
 {
@@ -15,6 +28,7 @@ int main(void)
 
 	// testHeartBeat();
 	testFlash();
+	// testDAC();
 
 	while(1);
 }
@@ -54,4 +68,84 @@ void testFlash()
 	SysCtlDelay(20000000);
 
 	while(1);
+}
+
+void testDAC()
+{
+	int data;
+	int datahi;
+	int datalo;
+	int PinData = 4;
+
+	// push button stuff
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+	GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
+
+	// I2C stuff
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+	GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
+	GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
+
+	GPIOPinConfigure(GPIO_PA6_I2C1SCL);
+	GPIOPinConfigure(GPIO_PA7_I2C1SDA);
+
+	I2CMasterInitExpClk(I2C1_MASTER_BASE, SysCtlClockGet(), false);
+	SysCtlDelay(10000);  // delay for SlaveAddrSet()
+
+	I2CMasterSlaveAddrSet(I2C1_MASTER_BASE, SLAVE_ADDRESS, false); // Write
+
+	//while(1)
+	//{
+	I2CMasterControl(I2C1_MASTER_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+	data = 0x98;
+	send_data(data); // slave address
+	data = 0x10;
+	send_data(data); // 0x10 updates register with data and outputs the data
+
+	datahi = 0x00;
+	datalo = 0x00;
+
+	while(1)
+	{
+		if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4))
+		{
+			if(datalo >= 0xFF)
+			{
+				if(datahi <= 0xFF)
+				{
+					datahi = datahi + 0x02;
+				}
+				else
+				{
+					datahi = 0x00;
+				}
+				datalo = 0x00;
+			}
+			else
+			{
+				datalo = datalo + 0x02;
+			}
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, PinData);
+		}
+		else
+		{
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00);
+		}
+		send_data(datahi);
+		send_data(datalo);
+	}
+
+	I2CMasterControl(I2C1_MASTER_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+}
+
+void send_data(int data)
+{
+	while(I2CMasterBusy(I2C1_MASTER_BASE)) {}
+	I2CMasterDataPut(I2C1_MASTER_BASE, data);
+	I2CMasterControl(I2C1_MASTER_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+	while(I2CMasterBusy(I2C1_MASTER_BASE)) {}
 }
